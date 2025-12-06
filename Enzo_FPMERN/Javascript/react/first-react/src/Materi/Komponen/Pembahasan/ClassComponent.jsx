ddf6d349ca073d76c921351542a3b99c557bff11
enzo <71802637+enzw@users.noreply.github.com>
2025-12-06 23:56:09 +0700

    add file materi component

---
+++ Enzo_FPMERN/Javascript/react/first-react/src/Materi/Komponen/Pembahasan/ClassComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class ClassComponent extends React.Component {
+    
+    
+    render() {
+        return(
+            <div>
+                <h1>Hello world</h1>
+                <h2>Komponen ini dibuat menggunakan ClassComponent</h2>
+            </div>
+        )
+    }
+}
+
+export { ClassComponent }
---
+++ Enzo_FPMERN/Javascript/react/first-react/src/Materi/Komponen/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ClassComponent } from "./Pembahasan/ClassComponent.jsx";
+
+class Komponen extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <ClassComponent/>
+            </div>
+        )
+    }
+}
+
