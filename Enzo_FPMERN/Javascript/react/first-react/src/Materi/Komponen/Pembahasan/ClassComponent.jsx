94b6e67b75c9fce9c7dd266402baad3a59f1399a
enzo <71802637+enzw@users.noreply.github.com>
2025-12-08 23:32:46 +0700

    move h1 to keep the git strike

--- Enzo_FPMERN/Javascript/react/first-react/src/Materi/Komponen/Pembahasan/ClassComponent.jsx
+++ Enzo_FPMERN/Javascript/react/first-react/src/Materi/Komponen/Pembahasan/ClassComponent.jsx
@@ -6,8 +6,8 @@ class ClassComponent extends React.Component {
     render() {
         return(
             <div>
-                <h1>Hello world</h1>
                 <h2>Komponen ini dibuat menggunakan Class Component</h2>
+                <h1>Hello world</h1>
             </div>
         )
     }
