8dd30433d704d5c8246af07051d4aed749b10d30
enzo <71802637+enzw@users.noreply.github.com>
2025-12-13 21:36:10 +0700

    add

---
+++ Enzo_FPMERN/Javascript/react/first-react/src/Materi/Styling/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sass from "./Sass/index";
+
+export default class Styling extends React.Component {
+    render() {
+        return(
+            <div>
+                <Sass />
+            </div>
+        )
+    }
+}
